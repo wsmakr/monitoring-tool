9f6b05800120d6e5c3c37f9ed6b121e7fa63b8f7
Wassim Akram <wassim.akram@stud.h-da.de>
2025-12-16 16:55:07 +0100

    feat: add data to a JSON File for persistance

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const fs = require('fs');
+const DATA_FILE = 'monitoring-data.json';
 
 // Environment variables of Node.js for modularity of monitoring-tool
 const CONFIG = {
@@ -6,27 +8,25 @@ const CONFIG = {
     checkInterval: parseInt(process.env.CHECK_INTERVAL) || 10000,
 };
 
-let checks = [];
+let checks = loadData();
 
-
+// Main function to check the website
 async function checkWebsite() {
     const startTime = Date.now();
 
     try {
         const response = await axios.get(CONFIG.url, {timeout: 5000});
         const duration = Date.now() - startTime;
-
         checks.push({
             timestamp: new Date().toISOString(),
             status: response.status,
             duration,
             success: true
         });
-
         console.log(`\`✅ ${new Date().toLocaleTimeString()} - Status: ${response.status} ${response.statusText} - Dutration: ${duration}ms`);
+        saveData()
     } catch (error) {
         const duration = Date.now() - startTime;
-
         checks.push({
             timestamp: new Date().toISOString(),
             status: error.response?.status || 'TIMEOUT',
@@ -34,18 +34,33 @@ async function checkWebsite() {
             success: false,
             error: error.message
         });
-
         console.log(`❌ ${new Date().toLocaleTimeString()} - Error: ${error.message}`);
+        saveData()
     }
 }
 
+// Get uptime of given website
 function getUptime() {
     if (checks.length === 0) return 0;
     const successful = checks.filter(statusReport => statusReport.success).length;
     return ((successful / checks.length) * 100).toFixed(2);
 }
 
+function saveData() {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(checks, null, 2));
+}
 
+function loadData() {
+    try {
+        const data = fs.readFileSync(DATA_FILE, 'utf8');
+        return JSON.parse(data);
+    } catch {
+        return [];
+    }
+}
+
+
+// Output for Console
 console.log(`🚀 Starting monitoring for: ${CONFIG.url}`);
 console.log(`📊 Check interval: ${CONFIG.checkInterval}ms\n`);
 
